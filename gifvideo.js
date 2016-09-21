fbe86b5a232be1dbe8ad0807f6bf452ac1abda58
Hay Kranen <huskyr@gmail.com>
2016-09-21 12:17:49 +0200

    Refactoring the code, and adding a second video

--- gifvideo.js
+++ gifvideo.js
@@ -1,81 +1,11 @@
 (function(root) {
-    function addCss(el) {
-        // Add something to use for the selector
-        var id = 'v' + String(Math.random()).slice(3);
-        el.setAttribute(id, '');
-
-        var css = document.createElement('style');
-        css.type = 'text/css';
-        var html = ''.concat(
-            '[' + id + ']::-webkit-media-controls-play-button,',
-            '[' + id + ']::-webkit-media-controls-start-playback-button {',
-            'opacity: 0;',
-            'pointer-events: none;',
-            'width: 5px;',
-            '}'
-        );
-        css.innerHTML = html;
-        document.body.appendChild(css);
-    }
-
-    function isIOS() {
-        return (/iP(hone|od|ad)/.test(navigator.platform));
-    }
-
-    function backgroundVideo(video) {
-        video.setAttribute('loop', '');
-
-        // We do some really ugly stuff to make sure this trick works on all
-        // platforms.
-        // First check for iOS, if we're not on that, simply return.
-        // Regular video playback is supported
-        if (!isIOS()) {
-            video.play();
-            return;
-        }
-
-        // Add the CSS to hide the play button
-        addCss(video);
-
-        // Now, on iOS < 10 we need the trick, otherwise we can simply use the
-        // webkit-playsinline property
-        // To detect that, we do a feature detect on a CSS property that was
-        // only added in iOS 10 Safari
-        if ('object-position' in document.head.style) {
-            video.setAttribute('webkit-playsinline', '');
-            video.setAttribute('playsinline', '');
-            video.setAttribute('muted', '');
-            video.play();
-            return;
-        }
-
-        // Okay, now get to the ugly hack for iOS < 10
-        var start;
-        var previousTime;
-        video.load();
-
-        // requestAnimationFrame automatically provides Date.now()
-        function play(now) {
-            // only play if it has the data,
-            // without waiting for ambiguous events
-            if (video.readyState >= video.HAVE_FUTURE_DATA) {
-                if (video.currentTime >= video.duration - 0.05) {
-                    // safer way to check whether the video is ended
-                    video.currentTime = 0; // restart video
-                } else {
-                    //make it start at 0
-                    previousTime = previousTime || now;
-                    // increase it by the time it passed since the last
-                    // animationFrame
-                    video.currentTime += (now - previousTime) / 1000;
-                }
-            }
-            previousTime = now;
-            requestAnimationFrame(play);
-        }
-
-        play(Date.now());
-    }
+    var VIDEO_ATTRIBUTES = [
+        'webkit-playsinline',
+        'playsinline',
+        'muted',
+        'loop',
+        'autoplay'
+    ];
 
     function Gifvideo(element) {
         if (typeof element !== 'object') {
@@ -83,12 +13,85 @@
         }
 
         this.element = element;
-        backgroundVideo(this.element);
+        this.addAttributes();
+        this.element.play();
+
+        if (this.isIos() && !this.isNewerIos()) {
+            // Here come the hacks
+            this.addCss();
+            this.scrubVideo();
+        }
     }
 
     Gifvideo.prototype = {
+        addAttributes : function() {
+            VIDEO_ATTRIBUTES.forEach(function(attr) {
+                this.element.setAttribute(attr, '');
+            }.bind(this));
+        },
+
+        addCss : function(el) {
+            // Removes the play button on iOS < 10
+            //
+            // Add something to use for the selector
+            var id = 'v' + String(Math.random()).slice(3);
+            el.setAttribute(id, '');
+
+            var css = document.createElement('style');
+            css.type = 'text/css';
+            var html = ''.concat(
+                '[' + id + ']::-webkit-media-controls-play-button,',
+                '[' + id + ']::-webkit-media-controls-start-playback-button {',
+                'opacity: 0;',
+                'pointer-events: none;',
+                'width: 5px;',
+                '}'
+            );
+            css.innerHTML = html;
+            document.body.appendChild(css);
+        },
+
         getElement : function() {
             return this.element;
+        },
+
+
+        isIos : function() {
+            return (/iP(hone|od|ad)/.test(navigator.platform));
+        },
+
+        isNewerIos : function() {
+            // This CSS property was only added in iOS 10
+            return 'object-position' in document.head.style;
+        },
+
+        // The ugly hack for iOS < 10
+        scrubVideo : function() {
+            var start;
+            var previousTime;
+            video.load();
+
+            // requestAnimationFrame automatically provides Date.now()
+            function play(now) {
+                // only play if it has the data,
+                // without waiting for ambiguous events
+                if (video.readyState >= video.HAVE_FUTURE_DATA) {
+                    if (video.currentTime >= video.duration - 0.05) {
+                        // safer way to check whether the video is ended
+                        video.currentTime = 0; // restart video
+                    } else {
+                        //make it start at 0
+                        previousTime = previousTime || now;
+                        // increase it by the time it passed since the last
+                        // animationFrame
+                        video.currentTime += (now - previousTime) / 1000;
+                    }
+                }
+                previousTime = now;
+                requestAnimationFrame(play);
+            }
+
+            play(Date.now());
         }
     };
 
